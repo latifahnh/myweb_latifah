f37c11ebaa2edf7974dd41f517e9f23c87436646
latifahnh <skyver.hans@gmail.com>
2019-12-12 21:52:37 +0700

    Edit commit 185411194

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $result = mysqli_query( $conn, "SELECT * FROM users ORDER BY id DESC");
 
 <html>
 <head>    
-    <title>Homepage</title>
+    <title>Ini Homepage untuk Tugas Git</title>
 </head>
 
 <body>
